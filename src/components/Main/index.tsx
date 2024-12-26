1aaee53cde163b5af9f91fe8ed93a46174ea6e1c
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2024-12-26 16:59:54 -0300

    feat: add styles

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,21 @@
+import * as mainS from '@/components/Main/styles/styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <mainS.Wrapper>
+    <mainS.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <mainS.Title>{title}</mainS.Title>
+    <mainS.Description>{description}</mainS.Description>
+    <mainS.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </mainS.Wrapper>
+)
+
+export default Main
