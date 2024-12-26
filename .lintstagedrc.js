24ea6a69652edb04d445da681206157cbbed1e12
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2024-12-26 16:35:45 -0300

    chore: add jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `pnpm run lint --fix  . ${filenames.join(' --file ')}`
-    //`pnpm run test -- --bail  --findRelatedTests --watchAll=false ${filenames.join(' ')}`
+    `pnpm run lint --fix  . ${filenames.join(' --file ')}`,
+    `pnpm run test -- --bail  --findRelatedTests --watchAll=false ${filenames.join(' ')}`
   ]
 }
---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+import nextJest from 'next/jest.js'
+import type { Config } from 'jest'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**', // should be tested in e2e
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/**/stories.tsx',
+    '!src/**/styles.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/tests/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  // https://github.com/styled-components/styled-components/issues/4081
+  // v6 of styled-components doesn't inject styles in test environment
+  // we should to force it to use the browser version
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
+  }
+}
+export default createJestConfig(config)
+//export default config
